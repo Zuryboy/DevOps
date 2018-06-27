d062b26e073ef33b8a8b92050090c6a5a3b8e79c
mzurita <migz87m@gmail.com>
2018-06-27 15:03:38 -0500

    Add Change13

--- deployments/qa.sh
+++ deployments/qa.sh
@@ -1,5 +1,5 @@
-sudo npm install -g now --unsafe-perm now
-echo "deploying..."
+npm install -g now --unsafe-perm now
+echo "Entro al archivo deploying..."
 URL= $(now --docker --public -t $NOW_TOKEN)
 echo "runnig acceptance on $URL"
 curl --silent -L $URL
